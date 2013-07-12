e1e7de09ae4f31434b6c62b1cb2a24f25c44ea57
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 11:59:38 -0700

    User login authenticates through controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,4 +4,17 @@ get '/' do
   haml :index, :layout_engine => :erb
 end
 
+get '/login' do
+  haml :login, :layout_engine => :erb, :layout => !request.xhr?
+end
 
+get 'signup' do
+  haml :signup, :layout_engine => :erb, :layout => !request.xhr?
+end
+
+post '/login' do
+  user = User.find_by_email(params[:login][:email])
+  if user.authenticate(params[:login][:password])
+    
+  end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#login').on('submit', function(e){
+    e.preventDefault();
+    $.post('/login', $(this).serialize()).done(function(response){
+      console.log(response);
+    });
+  });
 });
