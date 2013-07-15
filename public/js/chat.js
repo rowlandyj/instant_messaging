692d169f50bebf92b0673c9a6ace49bf6b2eb4b6
supertopher <asimplechris@gmail.com>
2013-07-14 18:47:33 -0700

    chat works on homebuilt websocket server... scrolling for chat window diabled.  not on this github

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,23 @@
-$(function() {
+$(function(){
+  var ENTER = 13;
+  var socket = new WebSocket('ws://tie-fighter.local:8090');
+  var chatLog = $('.chat_window')
+
+  socket.onmessage = function(event){
+    chatLog.append($('<p>' + event.data + '</p>'))
+    console.log(event.data);
+    chatLog.animate({scrollTop: chatLog.height()}, 1000)
+
+  };
+
+  $('.chat_input').keypress(function(event){
+    var el = $(this);
+
+    if (event.which == ENTER){
+      socket.send(el.val());
+      el.val('');
+    }
+  });
 
 
 });
